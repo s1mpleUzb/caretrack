d6d5c9abeeeff8ed1e413913f6ffc7fa598e2e15
fellerbr <fellerbr@student.21-school.ru>
2026-06-01 19:43:50 +0500

    fronted added

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 require("./db/schema");
-
 const doctorsRouter = require("./routes/doctors");
 const patientsRouter = require("./routes/patients");
 const diseasesRouter = require("./routes/diseases");
 const authRouter = require("./routes/auth");
 const reportsRouter = require("./routes/reports");
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "frontend")));
 
 app.get("/", (req, res) => {
-  res.send("CareTrack Backend Running");
+  res.sendFile(path.join(__dirname, "frontend", "login.html"));
 });
-
 app.use("/api/doctors", doctorsRouter);
 app.use("/api/patients", patientsRouter);
 app.use("/api/diseases", diseasesRouter);
