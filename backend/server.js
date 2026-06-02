d40f871feb3e9fad5e105b6f2593613639471749
fellerbr <fellerbr@student.21-school.ru>
2026-06-02 21:26:06 +0500

    fronted added finally 25

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
